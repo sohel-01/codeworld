3795c07e64682b31a37a3cba357d0aeb4b143796
MacStudent <macstudent@20X-iMacXX-M-477.local>
2018-06-01 14:57:03 -0400

    first time commit

---
+++ src/Employee.java
@@ -0,0 +1,25 @@
+package javaprogrammingi;
+
+
+public class Employee {
+    int employeeId;
+    String employeeName;
+
+    public int getEmployeeId() {
+        return employeeId;
+    }
+
+    public void setEmployeeId(int employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    public String getEmployeeName() {
+        return employeeName;
+    }
+
+    public void setEmployeeName(String employeeName) {
+        this.employeeName = employeeName;
+    }
+    
+    
+}
---
+++ src/javaprogrammingi/JavaProgrammingI.java
@@ -0,0 +1,13 @@
+package javaprogrammingi;
+
+
+public class JavaProgrammingI {
+
+    
+    public static void main(String[] args) {
+       
+        Employee e1 = new Employee();
+                
+    }
+    
+}
